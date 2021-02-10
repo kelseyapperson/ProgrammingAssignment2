fa008918656c70cb22a8469922460e88ec36c6f8
Kelsey Apperson <kapperson.dp@gmail.com>
2021-02-10 17:25:59 -0500

    Added comments and code sections for makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates object for matrix to be stored
+## cacheSolve caches the inverse solve value for later reference
 
-## Write a short comment describing this function
+#initialize variables, create get / set functionality
+#setmatrix / getmatrix use solve functions to get / set inverse matrix values
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+
+
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) i <<- solve
+  getmatrix <- function() i
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## caches the solve inverse matrix value for use later by referencing x$getmatrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getmatrix()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setmatrix(i)
+  i
+  
+  
 }
